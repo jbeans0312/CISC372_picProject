499270bbad4d8a5cdfd06d1695d748fbe0b2eeee
John Bean <jbeans@ubuntu-2004.eecis.udel.edu>
2023-04-19 14:16:08 -0400

    final commit for darwin

--- image_omp.c
+++ image_omp.c
@@ -2,6 +2,7 @@
 #include <time.h>
 #include <string.h>
 #include "image.h"
+#include <omp.h>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
@@ -58,6 +59,7 @@ uint8_t getPixelValue(Image* srcImage,int x,int y,int bit,Matrix algorithm){
 void convolute(Image* srcImage,Image* destImage,Matrix algorithm){
     int row,pix,bit,span;
     span=srcImage->bpp*srcImage->bpp;
+# pragma omp for
     for (row=0;row<srcImage->height;row++){
         for (pix=0;pix<srcImage->width;pix++){
             for (bit=0;bit<srcImage->bpp;bit++){
@@ -90,7 +92,7 @@ enum KernelTypes GetKernelType(char* type){
 //argv is expected to take 2 arguments.  First is the source file name (can be jpg, png, bmp, tga).  Second is the lower case name of the algorithm.
 int main(int argc,char** argv){
     long t1,t2;
-    t1=time(NULL);
+    t1=time(NULL);    
 
     stbi_set_flip_vertically_on_load(0); 
     if (argc!=3) return Usage();
@@ -111,12 +113,16 @@ int main(int argc,char** argv){
     destImage.height=srcImage.height;
     destImage.width=srcImage.width;
     destImage.data=malloc(sizeof(uint8_t)*destImage.width*destImage.bpp*destImage.height);
+    
+# pragma omp parallel
     convolute(&srcImage,&destImage,algorithms[type]);
+    t2 = time(NULL);
+    printf("Processing done, saving image\n");
+
     stbi_write_png("output.png",destImage.width,destImage.height,destImage.bpp,destImage.data,destImage.bpp*destImage.width);
     stbi_image_free(srcImage.data);
     
     free(destImage.data);
-    t2=time(NULL);
     printf("Took %ld seconds\n",t2-t1);
    return 0;
 }
